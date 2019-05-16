bba4a567bf2c9b9d6a1420c7ef2a8af56d05adf5
Quentin Dewaghe <q.dewaghe@googlemail.com>
2019-05-16 23:20:03 +0200

    fix

--- init.el
+++ init.el
@@ -1,26 +1,3 @@
 (require 'org) 
 (org-babel-load-file (expand-file-name (concat user-emacs-directory "settings.org")))
 
-;;Generated
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-<<<<<<< HEAD
-    ("6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" "4697a2d4afca3f5ed4fdf5f715e36a6cac5c6154e105f3596b44a4874ae52c45" default)))
-=======
-    ("f0dc4ddca147f3c7b1c7397141b888562a48d9888f1595d69572db73be99a024" "ecba61c2239fbef776a72b65295b88e5534e458dfe3e6d7d9f9cb353448a569e" "7e78a1030293619094ea6ae80a7579a562068087080e01c2b8b503b27900165c" "151bde695af0b0e69c3846500f58d9a0ca8cb2d447da68d7fbf4154dcf818ebc" "d2e9c7e31e574bf38f4b0fb927aaff20c1e5f92f72001102758005e53d77b8c9" "bf5bdab33a008333648512df0d2b9d9710bdfba12f6a768c7d2c438e1092b633" "6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" "fe666e5ac37c2dfcf80074e88b9252c71a22b6f5d2f566df9a7aa4f9bea55ef8" "b54826e5d9978d59f9e0a169bbd4739dd927eead3ef65f56786621b53c031a7c" "cd736a63aa586be066d5a1f0e51179239fe70e16a9f18991f6f5d99732cabb32" "4697a2d4afca3f5ed4fdf5f715e36a6cac5c6154e105f3596b44a4874ae52c45" default)))
->>>>>>> origin/master
- '(org-babel-load-languages (quote ((emacs-lisp . t) (C . t))))
- '(package-selected-packages
-   (quote
-    (ccls dired+ lua-mode doom-themes doom-modeline spaceline switch-window expand-region h diminish dashboard hungry-delete rainbow-delimiters rainbow-delimiters-mode rainbow-mode beacon eglot lsp-ui cquery counsel-projectile projectile yasnippet-snippets flycheck magit zenburn-theme which-key use-package try org-bullets org counsel color-theme auto-org-md auto-complete atom-one-dark-theme ace-window))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
