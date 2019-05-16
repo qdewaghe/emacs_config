9c8dcc0fa50d2179e9e07917d2886c597b8ec952
Quentin Dewaghe <q.dewaghe@googlemail.com>
2019-05-16 22:46:44 +0200

    replace eglot with lsp

--- init.el
+++ init.el
@@ -7,6 +7,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" "4697a2d4afca3f5ed4fdf5f715e36a6cac5c6154e105f3596b44a4874ae52c45" default)))
  '(org-babel-load-languages (quote ((emacs-lisp . t) (C . t))))
  '(package-selected-packages
    (quote
