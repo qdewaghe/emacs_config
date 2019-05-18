29dcce3a40dfe19e3b6bbdb1cf2584ce07fec666
Quentin Dewaghe <q.dewaghe@googlemail.com>
2019-05-18 13:11:43 +0200

    readded ccls path

--- transient/history.el
+++ transient/history.el
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
-((magit-diff
-  ("--no-ext-diff" "--stat")))
-=======
 ((magit-commit nil)
+ (magit-diff
+  ("--no-ext-diff" "--stat"))
  (magit-dispatch nil)
  (magit-fetch nil)
  (magit-merge nil)
  (magit-pull nil)
- (magit-push nil))
->>>>>>> origin/master
+ (magit-push nil)
+ (magit-stash nil)
+ (magit-tag nil))
