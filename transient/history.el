8294bd95f3f9ef08fe6c90155ffbf5dbb60ba9c8
Quentin Dewaghe <q.dewaghe@googlemail.com>
2019-05-18 15:41:05 +0200

    gitignore

--- transient/history.el
+++ transient/history.el
@@ -1,10 +1,6 @@
 ((magit-commit nil)
  (magit-diff
   ("--no-ext-diff" "--stat"))
- (magit-dispatch nil)
- (magit-fetch nil)
  (magit-merge nil)
  (magit-pull nil)
- (magit-push nil)
- (magit-stash nil)
- (magit-tag nil))
+ (magit-push nil))
