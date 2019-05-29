718cd9100532014840a7db24afc0eece8bb29b66
Quentin Dewaghe <q.dewaghe@googlemail.com>
2019-05-29 20:08:11 +0200

    cleaned up org

--- init.el
+++ init.el
@@ -15,4 +15,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(lsp-ui-doc-background ((t (:background nil))))
+ '(lsp-ui-doc-header ((t (:inherit (font-lock-string-face italic))))))
