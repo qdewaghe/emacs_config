23e0e094ffdcec726c900c8dd5a076e9ad4942e1
Quentin Dewaghe <quentin.dewaghe@amadeus.com>
2020-05-04 16:10:19 +0200

    auto guess root to nil

--- init.el
+++ init.el
@@ -8,14 +8,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-    ("43c808b039893c885bdeec885b4f7572141bd9392da7f0bd8d8346e02b2ec8da" default)))
+   '("43c808b039893c885bdeec885b4f7572141bd9392da7f0bd8d8346e02b2ec8da" default))
  '(package-selected-packages
-   (quote
-    (zzz-to-char cmake-mode cmake-ide dracula-theme clang-format yasnippet which-key use-package try switch-window spaceline rainbow-delimiters org-bullets modern-cpp-font-lock magit-popup magit lua-mode lsp-ui hungry-delete htmlize expand-region eglot doom-themes diminish dashboard cquery counsel company-lsp ccls beacon avy)))
- '(projectile-project-root-files-top-down-recurring
-   (quote
-    ("compile_commands.json" ".ccls" ".svn" "CVS" "Makefile"))))
+   '(flycheck dracula-theme clang-format yasnippet which-key use-package try switch-window spaceline rainbow-delimiters org-bullets modern-cpp-font-lock magit-popup magit lua-mode lsp-ui hungry-delete htmlize expand-region eglot doom-themes diminish dashboard cquery counsel company-lsp ccls beacon avy))
+ '(projectile-project-root-files-top-down-recurring '("compile_commands.json" ".ccls" ".svn" "CVS" "Makefile")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
